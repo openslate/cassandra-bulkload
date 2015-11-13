9415c16be5e642d1077c95188baabe9d8a8cde84
Mike O'Malley <spuriousdata@gmail.com>
2015-11-13 00:21:36 -0500

    upgrade csvparser version

--- load.groovy
+++ load.groovy
@@ -20,7 +20,7 @@
  */
 
 @Grab('org.apache.cassandra:cassandra-all:2.1.0')
-@Grab('com.xlson.groovycsv:groovycsv:1.0')
+@Grab('com.xlson.groovycsv:groovycsv:1.1')
 import static com.xlson.groovycsv.CsvParser.parseCsv
 
 import groovy.json.JsonSlurper
